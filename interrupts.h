b219d2ee3a9f99283ff63c66b601c72f6d6a4735
Manit-J <100145444+Manit-J@users.noreply.github.com>
2024-12-01 06:11:26 -0500

    Added methods for making memory status file

--- interrupts.h
+++ interrupts.h
@@ -31,4 +31,10 @@ process* removeProcess(process* head, process* p);
 
 process* addAtEnd(process* head, int pid, int memorySize, int arrivalTime, int totalCPUTime, int iof, int iod, int partitionNum, int runTime, int iocount, int iorunTime);
 
+process* createP(int pid, int memorySize, int arrivalTime, int totalCPUTime, int iof, int iod, int partitionNum, int runTime, int iocount, int iorunTime);
+
+char* memoryAsString();
+
+int getPartitionSize(int partitionNum);
+
 #endif
